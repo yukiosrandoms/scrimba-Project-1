c6a412a806f7aec1832cfdf2bc9450330ad122e3
Elsie Oduor <eoatieno@gmail.com>
2023-07-30 19:36:51 +0300

    Completed project 1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'))
+function MainContent(){
+  return(
+    <>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </>
+  )
+}
+
+ReactDOM.render(<MainContent />, document.getElementById('root'))
+
+ 
